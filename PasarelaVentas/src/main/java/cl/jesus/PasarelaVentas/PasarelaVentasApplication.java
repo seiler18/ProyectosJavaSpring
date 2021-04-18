d66f2b78db6a4d128e023a7161f5bde858e80006
Jesus Seiler <ichbinseiler@gmail.com>
2021-04-18 00:38:14 -0400

    creando proyectos spring

---
+++ PasarelaVentas/src/main/java/cl/jesus/PasarelaVentas/PasarelaVentasApplication.java
@@ -0,0 +1,13 @@
+package cl.jesus.PasarelaVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasarelaVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasarelaVentasApplication.class, args);
+	}
+
+}
